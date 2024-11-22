bc23f0244a30a943986dadb31d227bb5646db45e
bruno <bruno.aurelio89@hotmail.com>
2024-11-21 22:01:57 -0300

    feat-continuação dos exercicios

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,7 +4,7 @@ import inputData from "../support/functions"
 
 describe('Central de Atendimento ao Cliente TAT - Básico', ()=> {
 
-    context('Lessons 2', ()=>{
+    context.skip('Lessons 2', ()=>{
         beforeEach(()=> {
             cy.visit('./cypress-basico-v2/src/index.html')
         })
@@ -65,13 +65,83 @@ describe('Central de Atendimento ao Cliente TAT - Básico', ()=> {
             cy.get('.success').should('be.visible')
         });    
 
-        it('Ex. Extra 08 - ', () => {
-            
+        it('Ex. Extra 08 - Alterando de cy.get para cy.contains', () => {
+            cy.get('#firstName').type('Bruno')
+            cy.get('#lastName').type('Aurelio')
+            cy.get('#email').type('bruno.aurelio@email.com')
+            cy.get('#open-text-area').type(inputData.wordRandom(100), {delay:0})
+            cy.contains('button','Enviar').click() // Alterado somente esse item para exemplificar o Ex. 08
+            cy.get('.success').should('be.visible')
         });
         
     })
 
-    context('Lessons 3',()=>{
+    context.skip('Lessons 3 - Seleção do tipo Select (Dropbox)',()=>{
+        beforeEach(()=> {
+            cy.visit('./cypress-basico-v2/src/index.html')
+        })
 
+        it('Ex. 0 - Seleciona um produto (YouTube) por seu texto', () => {
+            cy.get('#product')
+                .select('YouTube')
+                .should('have.value','youtube')
+        });
+
+        it('Ex. Extra 01 - Seleciona um produto (Mentoria) por seu valor (value)', () => {
+            cy.get('#product')
+                .select('mentoria')
+                .should('have.value','mentoria')
+        });
+
+        it('Ex. Extra 02 - Seleciona um produto (Blog) por seu índice', () => {
+            cy.get('#product')
+                .select(1)
+                .should('have.value','blog')
+        });
+        
     })
+
+    context.skip('Lessons 4 - Seleção do tipo Radio',()=>{
+        beforeEach(()=> {
+            cy.visit('./cypress-basico-v2/src/index.html')
+        })
+
+        it('Ex. - Marca o tipo de atendimento "Feedback', () => {
+            cy.get('input[type=radio][value="feedback"]')
+                .check()
+                .should('have.value','feedback')
+        });
+
+        it('Ex. Extra - Marca cada tipo de atendimento', () => {
+            cy.get('input[type=radio]')
+                .should('have.length',3) //verifica se a quantidade de check na páginma = 3
+                .each(($radio)=>{
+                    cy.wrap($radio).check() // faz a marcação para cada um
+                    cy.wrap($radio).should('be.checked') // verifica cada um se foi realmente marcado
+                })
+        });
+    })
+
+    context('Lessons 5 - Marcar e desmarcar um checkbox', () => {
+        beforeEach(()=> {
+            cy.visit('./cypress-basico-v2/src/index.html')
+        })
+
+        it('Ex. 0 - Marca ambos checkboxes, depois desmarca o último', () => {
+            cy.get('input[type=checkbox]')
+                .check() //marca de forma automática ambos os checkbox, não necessita de selecionar cada um para ser marcado
+                .last()
+                .uncheck()
+                .should('not.be.checked')
+        });
+
+        it('Ex. 1 - Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+            
+        });
+
+        it('Ex. 0 - Marca ambos checkboxes, depois desmarca o último', () => {
+            cy.get('input[type=checkbox][name="email"]').check().should('be.checked')
+
+        });
+    });
   })
